d548eca6e2ede22f1ddaddadf83bbbfeeeac8ee1
Natee <gg977120@gmail.com>
2021-01-08 16:25:01 +0700

    Done!

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -10,6 +10,13 @@ int main (){
 	dest.open("cheerbook_copy.txt");
 	
 	//กรอก Code เฉพาะส่วนนี้ ให้สอดคล้องกับโจทย์และ Code ส่วนอื่น ๆ 
+	string textline;
+	dest << "-------------------- BOOM ---------------------" << endl;
+	while(getline(source,textline))
+	{
+		dest << textline << endl;
+	}
+	dest << "-------------------- HA!! ---------------------";
 	//โดยห้ามแก้ไข Code ในส่วนอื่น ๆ
 
     source.close();
--- lab9_3.cpp
+++ lab9_3.cpp
@@ -1,3 +1,35 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<cstdlib>
+#include<iomanip>
+#include<cmath>
+using namespace std;
+
+int main()
+{
+    int i = 0;
+    string textline;
+    double Mean = 0;
+    double Standard_deviation = 0;
+    ifstream source;
+    source.open("score.txt");
+
+    while (getline(source,textline))
+    {
+        Standard_deviation += pow(atof(textline.c_str()),2);
+        Mean += atof(textline.c_str());
+        cin.ignore();
+        i++;
+    }
+
+    Mean = Mean/i;
+    Standard_deviation = pow((Standard_deviation/i)-pow(Mean,2),0.5);
+
+    cout << "Number of data = " << i << endl;
+    cout << setprecision(3);
+    cout << "Mean = "<< Mean << endl;
+    cout << "Standard deviation = " << Standard_deviation;
+
+    return 0;
+}
